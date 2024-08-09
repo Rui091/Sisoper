4214d912043221190b574bdca9148aaf8649c8d7
Rui Yu Lei Wu <124305995+Rui091@users.noreply.github.com>
2024-08-09 10:19:40 -0500

    Update tarea.c
    punto4

--- tarea.c
+++ tarea.c
@@ -76,3 +76,23 @@ int main() {
     
     return 0;
 }
+
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int factorial(int n){
+    int ans=1;
+    int i;
+    for (i=1;i<=n;++i){
+        ans*=i;
+    }
+    return ans;
+}
+int main() {
+    int n;
+    printf("Digite un numero: ");
+    scanf("%d",&n);
+    printf("El factorial de %d es %d",n,factorial(n));
+
+    return 0;
+}
