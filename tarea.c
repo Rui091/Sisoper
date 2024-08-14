d416cc6270993b95f7a68250d8c40f7a81c89162
Rui Yu Lei Wu <124305995+Rui091@users.noreply.github.com>
2024-08-14 09:12:44 -0500

    Update tarea.c
    punto mayor

--- tarea.c
+++ tarea.c
@@ -96,3 +96,31 @@ int main() {
 
     return 0;
 }
+
+
+
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int max(int arr[3]){
+    int i;
+    int maxNum = arr[0];
+    for(i=1;i<3;++i){
+        if (maxNum < arr[i]){
+            maxNum = arr[i];
+        } 
+    }
+    return maxNum;
+}
+
+int main() {
+    int i;
+    int arr[3];
+    for(i=0;i<3;++i){
+        printf("Ingrese el numero %d: ",i+1);
+        scanf("%d",&arr[i]);
+    }
+    printf("El mayor es: %d",max(arr));
+    
+
+}
