5905b45696c385c375616ed57a34b9bdfc6897cc
Rui Yu Lei Wu <124305995+Rui091@users.noreply.github.com>
2024-08-14 09:22:37 -0500

    Update tarea.c

--- tarea.c
+++ tarea.c
@@ -124,6 +124,34 @@ int main() {
         scanf("%d",&arr[i]);
     }
     printf("El mayor es: %d",max(arr));
+
+// Online C compiler to run C program online
+#include <stdio.h>
+
+    
+void imprimirFibonacciHasta(int n) {
+    if (n <= 0) {
+        printf("El número debe ser mayor que 0.\n");
+        return;
+    }
+    // Inicializar los primeros dos números de la serie de Fibonacci
+    int a = 0, b = 1;
+    // Imprimir los números de Fibonacci hasta que el siguiente número sea mayor que n
+    while (a <= n) {
+        printf("%d ", a);
+        int siguiente = a + b;
+        a = b;
+        b = siguiente;
+    }
+    printf("\n");
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    imprimirFibonacciHasta(n);
+    return 0;
+}
     
 
 }
